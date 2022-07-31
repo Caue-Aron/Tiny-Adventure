ed61a820de2a87b0bbb7e59b3600dcac5f85a19a
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2022-07-31 17:01:35 -0300

    First Commit

---
+++ general/general.lua
@@ -0,0 +1,23 @@
+key = {
+  left    = hash "left",
+  right   = hash "right",
+  up      = hash "up",
+  down    = hash "down",
+  action  = hash "action",
+  item1   = hash "item1",
+  item2   = hash "item2",
+  attack  = hash "attack",
+}
+
+collisions = {
+  map_terrain = hash 'map-terrain',
+  deep_water = hash 'deep-water',
+  fall_area = hash 'fall-area',
+  shallow_water = hash 'shallow-water',
+  hole_chaminea = hash 'hole-chaminea',
+  stone = hash 'stone',
+  tall_grass = hash 'tall-grass',
+}
+
+camera_center_x = 104
+camera_center_y = 64

2e70a084ebaafd06d4d91ddadf28c15f0664e678
Cauê <54822334+Caue-Aron@users.noreply.github.com>
2022-08-01 23:18:02 -0300

    Ready to merge to main

---
+++ Assets/libs/orthographic/camera.go
@@ -0,0 +1,45 @@
+components {
+  id: "camera"
+  component: "/Assets/libs/orthographic/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "center_x"
+    value: "80.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "center_y"
+    value: "75.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "near_z"
+    value: "-10.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "far_z"
+    value: "10.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "zoom"
+    value: "5.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "projection"
+    value: "FIXED_ZOOM"
+    type: PROPERTY_TYPE_HASH
+  }
+}
